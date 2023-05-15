1b1488753bbe3e076a6fac23f283c21640d3b024
oladsdan <104092480+oladsdan@users.noreply.github.com>
2023-05-15 10:15:48 +0100

    searchbar components

---
+++ src/components/VideosDisplay.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import { Stack, Box } from '@mui/material';
+import { VideoCard, ChannelCard} from './'
+
+const VideosDisplay = ({ videos }) => {
+ 
+  return (
+   <Stack direction="row" flexWrap="wrap" justifyContent="start" gap={2}>
+    {videos.map((item, idx) => (
+        <Box key={idx}>
+            {/* this assures that is a video properties */}
+            {item.id.videoId && <VideoCard video={item} />}
+            {/* this ensures that it can be a channel */}
+            {item.id.channelId && <ChannelCard channelDetail={item} />}
+
+        </Box>
+    ))}
+
+   </Stack>
+  )
+}
+
+export default VideosDisplay 
